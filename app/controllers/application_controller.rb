aa7593627284e6c37ec6d6910951257eb091307b
Ben Purinton <ben.purinton@gmail.com>
2023-04-28 15:28:17 -0800

    change comment in application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < Sinatra::Base
-  # # first we import sinatra so we have access to its 
-  # # objects and methods
+  # we not longer need to require sinatra
   # require 'sinatra'
 
   get('/') do
