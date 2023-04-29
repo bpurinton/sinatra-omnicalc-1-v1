51bf5423233fa9664d603001e422c769969a6052
Ben Purinton <ben.purinton@gmail.com>
2023-04-28 16:20:42 -0800

    spacing in environment

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,11 @@
 require 'sinatra'
+
 app_directory = File.join(__dir__, '..', 'app')
+
 ruby_files = File.join(app_directory, '**', '*.rb')
+
 ruby_files_paths = Dir.glob(ruby_files)
+
 ruby_files_paths.each do |file|
    require file
 end
