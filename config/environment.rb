4bc05a0ba2def03a5eb8d2e3710546f22a950ee6
Ben Purinton <ben.purinton@gmail.com>
2023-04-28 16:05:51 -0800

    require app directory in environment

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,12 @@
-require './app'
+require 'sinatra'
+app_directory = File.join(__dir__, '..', 'app')
+ruby_files = File.join(app_directory, '**', '*.rb')
+ruby_files_paths = Dir.glob(ruby_files)
+ruby_files_paths.each do |file|
+   require file
+end
 
 configure do
-  # instruct the app where to find the view templates
-  set :views, "app/views"
-
   # To open .html.erb files, need to register them
   Tilt.register(Tilt::ERBTemplate, 'html.erb')
   
