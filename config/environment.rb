a0ab08c0d757f9313d3ac07de7998e89b8a54e6f
Ben Purinton <ben.purinton@gmail.com>
2023-04-29 12:39:32 -0800

    wrap config in app class to get it working

--- config/environment.rb
+++ config/environment.rb
@@ -1,11 +1,34 @@
 require 'sinatra'
 
-app_directory = File.join(__dir__, '..', 'app')
+# we need to wrap the configuration settings in an app class
+class MyApp < Sinatra::Base
 
-ruby_files = File.join(app_directory, '**', '*.rb')
+  configure do
+    # we need to specify where the views are
+    set(:views, "app/views")
+    
+    # To open .html.erb files, need to register them
+    Tilt.register(Tilt::ERBTemplate, 'html.erb')
+    
+    # alternative to setting the layout in every erb() call
+    # we can set the default layout name to use and sinatra 
+    # will look for that file
+    set(:erb, :layout => :application_layout)
+  end
 
-ruby_files_paths = Dir.glob(ruby_files)
+  configure :development do
+    require "sinatra/reloader"
 
-ruby_files_paths.each do |file|
-   require file
+    # we would also like a nicer error page in development
+    require 'better_errors'
+    require 'binding_of_caller'
+    
+    # need this configure for better errors
+    use(BetterErrors::Middleware)
+    BetterErrors.application_root = __dir__
+    BetterErrors::Middleware.allow_ip!("0.0.0.0/0.0.0.0")
+  end
+
 end
+
+require "./app/controllers/application_controller"
