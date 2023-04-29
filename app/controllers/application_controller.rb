97d1d94074b052bcc1d46d2eac14fa453b3711c8
Ben Purinton <ben.purinton@gmail.com>
2023-04-28 16:20:00 -0800

    layout working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,32 @@ class ApplicationController < Sinatra::Base
   # we not longer need to require sinatra
   # require 'sinatra'
 
-  # we need to specify where the views are
-  views_directory = File.join(__dir__, '..', 'views')
-  set(:views, views_directory)
+  configure do
+    # we need to specify where the views are
+    views_directory = File.join(__dir__, '..', 'views')
+    set(:views, views_directory)
+    
+    # To open .html.erb files, need to register them
+    Tilt.register(Tilt::ERBTemplate, 'html.erb')
+    
+    # alternative to setting the layout in every erb() call
+    # we can set the default layout name to use and sinatra 
+    # will look for that file
+    set(:erb, :layout => :application_layout)
+  end
+  
+  configure :development do
+    require "sinatra/reloader"
+  
+    # we would also like a nicer error page in development
+    require 'better_errors'
+    require 'binding_of_caller'
+    
+    # need this configure for better errors
+    use(BetterErrors::Middleware)
+    BetterErrors.application_root = __dir__
+    BetterErrors::Middleware.allow_ip!("0.0.0.0/0.0.0.0")
+  end
 
   get('/') do
     "
